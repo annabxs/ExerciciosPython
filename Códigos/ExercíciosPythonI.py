f7d64cdf987d6f8dd09f9071e30c767afd96b9f0
unknown <2mtec@e041edu.local>
2026-03-26 10:38:37 -0300

    6 - Fazendo exercício 10

--- Códigos/ExercíciosPythonI.py
+++ Códigos/ExercíciosPythonI.py
@@ -86,3 +86,33 @@ while contador <= 5:
     contador += 1
     soma += num
 print("A soma dos números é: ", soma)
+
+#9
+
+
+
+
+
+
+
+#10
+media = 0
+soma = 0
+i = 0
+
+while i < 3:
+    nota = float(input("Digite sua nota: "))
+    soma += nota
+    i += 1
+    
+media = soma/3
+
+if media >= 8:
+    conceito = "A"
+elif media >=5 and media < 8:
+    conceito = "B"
+else:
+    conceito = "C"
+    
+print("Conceito: {}\n"
+      "Média: {:.2f}".format(conceito, media))
